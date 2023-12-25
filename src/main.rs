21b52f0a27c068ef71f4249c63ebf3c37a96e597
Daniel Schauenberg <d@unwiredcouch.com>
2023-12-25 22:01:26 +0100

    clean up some args we don't need

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,6 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
     command: Option<Commands>,
 }

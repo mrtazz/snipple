0743e359c906ab97f2374e0715d56a0782702190
Daniel Schauenberg <d@unwiredcouch.com>
2023-12-27 19:17:00 +0100

    restructure code a bit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
 use std::env::VarError;
-use std::fs;
 
 use clap::{Parser, Subcommand};
 
-const SNIPPET_SUFFIX: &str = ".snippet";
+mod snipple;
+
 const DEFAULT_SNIPPET_LOCATION: &str = "~/.snippets";
 const SNIPPLE_VERSION: Option<&'static str> = option_env!("SNIPPLE_VERSION");
 
@@ -34,35 +34,34 @@ fn main() {
 
     match &cli.command {
         Some(Commands::ListSnippets {}) => {
-            let location =
-                resolve_tilde_in_snippet_location(std::env::var("HOME"), DEFAULT_SNIPPET_LOCATION);
+            let location = get_snippet_base_dir(std::env::var("HOME"), DEFAULT_SNIPPET_LOCATION);
             match location {
                 Ok(location) => {
-                    let snippets = find_all_snippets_in_directory(location.as_str()).unwrap();
+                    let m = snipple::Manager::new(location);
+                    let snippets = m.list_all_snippets().unwrap();
                     for snippet in snippets {
-                        match snippet
-                            .as_str()
-                            .strip_prefix(format!("{}/", location).as_str())
-                        {
-                            Some(snippet) => {
-                                println!("{}", snippet)
-                            }
-                            None => {}
-                        }
+                        println!("{}", snippet)
                     }
                 }
                 Err(_) => {}
             }
         }
         Some(Commands::Get { snippet }) => {
-            let snippet = get_snippet(snippet);
-            match snippet {
-                Ok(snippet) => {
-                    println!("{}", snippet);
+            let location = get_snippet_base_dir(std::env::var("HOME"), DEFAULT_SNIPPET_LOCATION);
+            match location {
+                Ok(location) => {
+                    let m = snipple::Manager::new(location);
+                    let snippet = m.get_snippet(snippet);
+                    match snippet {
+                        Ok(snippet) => {
+                            println!("{}", snippet);
+                        }
+                        Err(e) => {
+                            println!("error getting snippet: {}", e.to_string())
+                        }
+                    }
                 }
-                Err(e) => {
-                    println!("error getting snippet: {}", e.to_string())
-                }
+                Err(_) => {}
             }
         }
         Some(Commands::Version {}) => {
@@ -80,10 +79,7 @@ fn main() {
     }
 }
 
-fn resolve_tilde_in_snippet_location(
-    home: Result<String, VarError>,
-    location: &str,
-) -> Result<String, String> {
+fn get_snippet_base_dir(home: Result<String, VarError>, location: &str) -> Result<String, String> {
     if location.starts_with("~/") {
         match home {
             Ok(home) => {
@@ -97,89 +93,13 @@ fn resolve_tilde_in_snippet_location(
     }
 }
 
-fn get_snippet(path: &str) -> Result<String, String> {
-    let location =
-        resolve_tilde_in_snippet_location(std::env::var("HOME"), DEFAULT_SNIPPET_LOCATION);
-    match location {
-        Ok(location) => match fs::read_to_string(format!("{}/{}", location, path)) {
-            Ok(contents) => Ok(String::from(contents)),
-            Err(e) => Err(format!(
-                "should have been able to read file '{}', got error '{}'",
-                path, e
-            )),
-        },
-        Err(e) => Err(format!(
-            "couldn't resolve tilde in snippet location: {}",
-            e.to_string()
-        )),
-    }
-}
-
-fn find_all_snippets_in_directory(dir: &str) -> Result<Vec<String>, String> {
-    let mut snippets: Vec<String> = Vec::new();
-    let paths = fs::read_dir(dir).unwrap();
-
-    for path in paths {
-        match path {
-            Ok(path) => {
-                if path.path().is_dir() {
-                    let mut this_snippets =
-                        find_all_snippets_in_directory(path.path().to_str().unwrap());
-                    match this_snippets.as_mut() {
-                        Ok(this_snippets) => {
-                            snippets.append(this_snippets);
-                        }
-                        Err(e) => {
-                            println!(
-                                "Failed to get snippets in directory. Error: {}",
-                                e.to_string()
-                            )
-                        }
-                    }
-                } else {
-                    match path.path().to_str() {
-                        Some(filename) => {
-                            if filename.ends_with(SNIPPET_SUFFIX) {
-                                snippets.push(String::from(filename));
-                            }
-                        }
-
-                        None => {
-                            println!("path isn't valid unicode")
-                        }
-                    }
-                }
-            }
-            //
-            Err(e) => {
-                println!("encountered error: {}", e.to_string());
-                continue;
-            }
-        }
-    }
-
-    return Ok(snippets);
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_find_all_snippets_in_directory() {
-        let snippets = find_all_snippets_in_directory("./fixtures");
-        assert_eq!(snippets.unwrap().len(), 2);
-    }
-    #[test]
-    fn test_resolve_tilde_in_snippet_location() {
-        let location = resolve_tilde_in_snippet_location(
-            Ok(String::from("/home/mrtazz")),
-            DEFAULT_SNIPPET_LOCATION,
-        );
+    fn test_get_snippet_base_dir() {
+        let location =
+            get_snippet_base_dir(Ok(String::from("/home/mrtazz")), DEFAULT_SNIPPET_LOCATION);
         assert_eq!(location.unwrap(), "/home/mrtazz/.snippets");
-    }
-    #[test]
-    fn test_get_snippet() {
-        let snippet = get_snippet("./fixtures/hello.snippet");
-        assert_eq!(snippet.unwrap(), "Hello");
     }
 }
